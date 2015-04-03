2a7db7288c84a86bd77a2c47504975ced4483c66
jmunoz <jpmc.pe@gmail.com>
2015-04-03 10:22:34 -0500

    se corrigio funcion SplitBillTestCase en test.py

--- python_test/test.py
+++ python_test/test.py
@@ -22,8 +22,10 @@ def split_bill(price, discount, people):
 
     new_price = price * (1 - (discount / float(100)))
     list_price = list()
-    for person in people:
-        list_price.append(new_price * person)
+    for person in people[:-1]:
+        list_price.append(float("%.2f" % (new_price * person)))
+    list_price.append(float("%.2f" % (new_price-sum(list_price))))
+
     return list_price
 
 
@@ -33,20 +35,17 @@ class SplitBillTestCase(unittest.TestCase):
 
     def test_wrong_split(self):
         s = split_bill(price=149.99, discount=15, people=[(1.0 / 2), (1.0 / 6), (1.0 / 6), (1.0 / 6)])
-        s =  round(sum(s), 2)
-        self.assertEquals(s, 127.49)
+        self.assertEquals(s, [63.75, 21.25, 21.25, 21.24])
 
     def test_right_sum(self):
         s = split_bill(price=149.99, discount=15, people=[(1.0 / 7), (2.0 / 7), (1.0 / 7), (3.0 / 7)])
-        s = round(sum(s), 2)
-        self.assertEquals(s, 127.49)
+        self.assertEquals(s, [18.21, 36.43, 18.21, 54.64])
 
     def test_wrong_sum(self):
         s = split_bill(price=1, discount=0,
                        people=[(1.0 / 10), (1.0 / 10), (1.0 / 10), (1.0 / 10), (1.0 / 10),
                                (1.0 / 10), (1.0 / 10), (1.0 / 10), (1.0 / 10), (1.0 / 10)])
-        s = round(sum(s), 2)
-        self.assertEquals(s, 1)
+        self.assertEquals(s, [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
 
 import sys
 import getopt
@@ -58,6 +57,7 @@ class Usage(Exception):
 
 
 def main(argv=None):
+
     if argv is None:
         argv = sys.argv
     try:
